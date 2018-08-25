c3ec425a54a3316c2ecf06599d3e6dd593beeb5a
hgryoo <hyunggyu.ryoo@gmail.com>
2018-08-26 04:34:32 +0900

    added shell to install boost in Windows

--- sfcgal4j-impl/cppbuild.sh
+++ sfcgal4j-impl/cppbuild.sh
@@ -148,6 +148,79 @@ case $OPERATION in
 				cd ../
 			fi
 			;;
+			windows-x86_64)
+			    	download https://sourceforge.net/projects/boost/files/boost/1.67.0/boost_1_67_0.zip/download boost.zip
+			    	download https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.11.1/CGAL-4.11.1.zip CGAL.zip
+			    	download https://github.com/Oslandia/SFCGAL/archive/v1.3.5.zip SFCGAL-1.3.5.zip
+					
+					echo "Unzipping boost.zip"
+			    	unzip -qo ../downloads/boost.zip
+			    	
+			    	echo "Unzipping CGAL.zip"
+					unzip -qo ../downloads/CGAL.zip
+			    	
+			    	echo "Unzipping SFCGAL.zip"
+			    	unzip -qo ../downloads/SFCGAL.zip
+			    	
+			    	#download https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.10.1/CGAL-4.10.1-Setup.exe CGAL-4.10.1-Setup.exe
+			    	
+			    	#echo "Unzipping CGAL-4.10.1.zip"
+			    	#unzip -z -n ../downloads/CGAL-4.10.1.zip
+			    	#../downloads/CGAL-4.10.1-Setup.exe
+			    	
+			    	# building boost
+			    	cd boost_1_67_0/
+			        ./bootstrap.bat "--prefix=$INSTALL_PATH" "--with-libraries=filesystem,system,thread,date_time,serialization,timer"
+			        ./b2 "--prefix=$INSTALL_PATH" "link=static,shared" "address-model=64" install
+			        cd ../
+					
+			        #export BOOST_BUILD_PATH="$INSTALL_PATH/boost_1_63_0/boost_build/bin"
+			        #cd ../
+			        #cd ../../
+			        #$INSTALL_PATH/boost_1_63_0/boost_build/bin/b2 "--prefix=$INSTALL_PATH" link=shared "address-model=64" "toolset=gcc" install
+			        #cd ../../../
+					#cd ../
+					#ln -sf libboost_thread.a lib/libboost_thread-mt.a
+					
+					#cd boost_1_63_0/
+			        #./bootstrap.bat g++
+			        #./b2 clean
+					#./b2 -j4 -a --toolset=g++ "--prefix=$INSTALL_PATH" variant=release link=shared threading=multi address-model=64
+			        #cd ../
+
+					ln -sf libboost_thread-mgw72-mt-1_63.dll lib/libboost_thread-mt.dll
+					
+					export BOOST_ROOT="$INSTALL_PATH/boost_1_63_0"		
+					export BOOST_INCLUDEDIR="$INSTALL_PATH/include/boost"
+					export BOOST_LIBRARYDDIR="$INSTALL_PATH/lib/"
+					export BOOST_THREAD_LIBRARY_RELEASE="$INSTALL_PATH/lib/libboost_thread-mgw72-mt-1_63.dll"
+			        
+			        export GMP_DIR="$INSTALL_PATH/CGAL-4.10.1/auxiliary/gmp/"
+			        export GMP_INC_DIR="$INSTALL_PATH/CGAL-4.10.1/auxiliary/gmp/include"
+			        export GMP_LIB_DIR="$INSTALL_PATH/CGAL-4.10.1/auxiliary/gmp/lib"
+			        
+			        export MPFR_INC_DIR="$INSTALL_PATH/CGAL-4.10.1/auxiliary/gmp/include"
+			        export MPFR_LIB_DIR="$INSTALL_PATH/CGAL-4.10.1/auxiliary/gmp/lib"
+			        
+			        export CMAKE_CXX_COMPILER=g++;
+			        export CMAKE_C_COMPILER=gcc;
+			        
+			        # building cgal
+			        cd CGAL-4.10.1/
+			        cmake -G"MinGW Makefiles" "-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH" "-DBUILD_SHARED_LIBS=TRUE"
+			        mingw32-make
+			        cd ../
+			        
+			        # building sfcgal
+			        cd SFCGAL-1.3.2/
+			        
+			        export CGAL_DIR="$INSTALL_PATH/CGAL-4.10.1"
+			        export CGAL_INCLUDE_DIRS="$INSTALL_PATH/CGAL-4.10.1/include"
+			        
+			        cmake -G "MinGW Makefiles" "-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH" "-DSFCGAL_USE_STATIC_LIBS=OFF"
+			        mingw32-make
+			        cd ../
+			   	;;
 	    	*)
 		        echo "Error: Platform \"$PLATFORM\" is not supported"
 		        ;;
